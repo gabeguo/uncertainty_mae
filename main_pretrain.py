9f3c0ff1174de478d69d57428cdd2cb40c1b39c6
gabeguo <gzg2104@columbia.edu>
2023-12-08 18:06:55 -0500

    script works with mnist now

--- main_pretrain.py
+++ main_pretrain.py
@@ -125,7 +125,9 @@ def main(args):
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-    dataset_train = datasets.ImageFolder(os.path.join(args.data_path, 'train'), transform=transform_train)
+    dataset_train = torchvision.datasets.MNIST(root=args.data_path, train=True, download=True, 
+        target_transform=transform_train)
+    #dataset_train = datasets.ImageFolder(os.path.join(args.data_path, 'train'), transform=transform_train)
     print(dataset_train)
 
     if True:  # args.distributed:
---
+++ train_model.sh
@@ -0,0 +1,2 @@
+python main_pretrain.py \
+    --data_path /home/gabeguo/data/vae_mae/mnist_pytorch_data
