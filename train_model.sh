6e36d962884ecc8bbb8aa1ea094516a3a516cb75
gabeguo <gzg2104@columbia.edu>
2023-12-13 15:09:34 -0500

    specified log and output dir

--- train_model.sh
+++ train_model.sh
@@ -1,5 +1,6 @@
 python main_pretrain.py \
     --data_path /home/gabeguo/data \
     --accum_iter 1 \
-    --output_dir v1 \
+    --output_dir emnist \
+    --log_dir emnist \
     --model mae_vit_base_patch16 
