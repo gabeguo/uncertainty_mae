3340dc4eaa115ccb53b9da595790e7b203cd4df0
Gabe Guo <gzg2104@columbia.edu>
2024-03-12 12:18:11 -0400

    commencing pre-training

--- train_model.sh
+++ train_model.sh
@@ -2,6 +2,6 @@ python main_pretrain.py \
     --data_path /home/gabeguo/data \
     --batch_size 64 \
     --accum_iter 1 \
-    --output_dir omniglot \
-    --log_dir omniglot \
+    --output_dir cifar100_train \
+    --log_dir cifar100_train \
     --model mae_vit_base_patch16 
