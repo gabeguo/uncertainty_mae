3df1cbcfd29902f2f18d487d3eac4568c43b7a0f
Gabe Guo <gzg2104@columbia.edu>
2024-03-12 12:18:49 -0400

    changed output dir

--- linprobe.sh
+++ linprobe.sh
@@ -3,5 +3,6 @@ python main_linprobe.py \
     --finetune /home/gabeguo/vae_mae/mae_visualize_vit_base.pth \
     --data_path /home/gabeguo/data/cifar-100-python \
     --nb_classes 100 \
+    --output_dir cifar100_finetune \
     --device cuda
 
