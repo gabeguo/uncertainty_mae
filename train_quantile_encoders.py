b7d2addcece832dc4df6fbbfa2954a4ba84f49e6
Gabe Guo <gzg2104@columbia.edu>
2024-03-14 15:31:10 -0400

    changed default epochs

--- train_quantile_encoders.py
+++ train_quantile_encoders.py
@@ -110,7 +110,7 @@ def get_args_parser():
                         help='dataset path')
     parser.add_argument('--batch_size', default=64, type=int,
                         help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
-    parser.add_argument('--epochs', default=200, type=int)
+    parser.add_argument('--epochs', default=50, type=int)
     parser.add_argument('--accum_iter', default=1, type=int,
                         help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
     parser.add_argument('--input_size', default=224, type=int,
