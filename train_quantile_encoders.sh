8b9ab0cab291da81f8edbcdff7206ebd60a41c3e
Gabe Guo <gzg2104@columbia.edu>
2024-03-14 18:35:42 -0400

    added path to train script

--- train_quantile_encoders.sh
+++ train_quantile_encoders.sh
@@ -1,4 +1,5 @@
 python train_quantile_encoders.py \
     --lr 1e-4 \
     --min_lr 1e-7 \
-    --num_unshared_layers 3
+    --num_unshared_layers 3 \
+    --pretrained_weights /home/gabeguo/uncertainty_mae/cifar100_train/checkpoint-399.pth
