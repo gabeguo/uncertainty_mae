f7e145a6f729cd969a67c9ffccebb073003fc317
Gabe Guo <gzg2104@columbia.edu>
2024-03-16 21:26:50 -0400

    train quantile encoders

--- train_quantile_encoders.sh
+++ train_quantile_encoders.sh
@@ -2,4 +2,5 @@ python train_quantile_encoders.py \
     --lr 1e-4 \
     --min_lr 1e-7 \
     --num_unshared_layers 3 \
-    --pretrained_weights /home/gabeguo/uncertainty_mae/cifar100_train/checkpoint-399.pth
+    --pretrained_weights /home/gabeguo/uncertainty_mae/cifar100_train/checkpoint-399.pth \
+    --output_dir cifar100_quantile
