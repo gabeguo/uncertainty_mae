ea8dbb80e7d0a2a545806535454e459707aa51f2
Gabe Guo <gzg2104@columbia.edu>
2024-03-18 10:53:24 -0400

    version of quantile encoders that trains all tokens

---
+++ train_quantile_encoders_all_tokens.sh
@@ -0,0 +1,8 @@
+python train_quantile_encoders.py \
+    --lr 1e-3 \
+    --min_lr 1e-7 \
+    --pretrained_weights /home/gabeguo/uncertainty_mae/cifar100_train/checkpoint-399.pth \
+    --output_dir cifar100_quantile_allTokens \
+    --batch_size 64 \
+    --mask_ratio 0.75 \
+    --return_all_tokens
