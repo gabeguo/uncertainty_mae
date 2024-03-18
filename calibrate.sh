eb082b5be195541225bd395f828e69c275541bca
Gabe Guo <gzg2104@columbia.edu>
2024-03-18 00:11:03 -0400

    updated calibration script

--- calibrate.sh
+++ calibrate.sh
@@ -1,9 +1,9 @@
 python calibrate.py \
-    --lower_model_filepath /home/gabeguo/uncertainty_mae/cifar100_quantile/lower_bound_checkpoint-0.pt \
-    --upper_model_filepath /home/gabeguo/uncertainty_mae/cifar100_quantile/upper_bound_checkpoint-0.pt \
-    --point_model_filepath /home/gabeguo/uncertainty_mae/cifar100_quantile/median_bound_checkpoint-0.pt \
+    --lower_model_filepath /home/gabeguo/uncertainty_mae/cifar100_quantile/lower_encoder_mae.pt \
+    --upper_model_filepath /home/gabeguo/uncertainty_mae/cifar100_quantile/upper_encoder_mae.pt \
+    --point_model_filepath /home/gabeguo/uncertainty_mae/cifar100_quantile/median_encoder_mae.pt \
     --gt_model_filepath /home/gabeguo/uncertainty_mae/cifar100_train/checkpoint-399.pth \
     --mask_ratio 0.75 \
     --batch_size 256 \
-    --max_scale_factor 1 \
+    --max_scale_factor 2 \
     --step_size 0.1
