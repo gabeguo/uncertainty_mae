9227ae6277b8885ae05febc8d6d55d531e024164
Gabe Guo <gzg2104@columbia.edu>
2024-03-23 23:24:22 -0400

    Have code to finetune median representations

--- finetune_quantile.sh
+++ finetune_quantile.sh
@@ -17,3 +17,13 @@ python main_finetune.py \
     --batch_size 128 \
     --epochs 100 \
     --device cuda
+
+python main_finetune.py \
+    --model vit_base_patch16 \
+    --finetune /home/gabeguo/uncertainty_mae/cifar100_train_median/checkpoint-399.pth \
+    --data_path /home/gabeguo/data/cifar-100-python \
+    --nb_classes 100 \
+    --output_dir cifar100_finetune_median \
+    --batch_size 128 \
+    --epochs 100 \
+    --device cuda
