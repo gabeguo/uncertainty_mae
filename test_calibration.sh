0804144efbebc0efad5be390497f77b454c96dec
Gabe Guo <gzg2104@columbia.edu>
2024-03-24 11:53:53 -0400

    test calibration of upper

--- test_calibration.sh
+++ test_calibration.sh
@@ -1,10 +1,20 @@
 python test_calibration.py \
-    --eval_weights /home/gabeguo/uncertainty_mae/cifar100_linprobe_uncertainty_revised/checkpoint-85.pth \
-    --use_ci
-
-python test_calibration.py \
     --eval_weights /home/gabeguo/uncertainty_mae/cifar100_linprobe_point/checkpoint-89.pth
 
 python test_calibration.py \
     --eval_weights /home/gabeguo/uncertainty_mae/cifar100_finetune_point/checkpoint-99.pth \
+    --global_pool
+
+python test_calibration.py \
+    --eval_weights /home/gabeguo/uncertainty_mae/cifar100_linprobe_upper/checkpoint-85.pth
+
+python test_calibration.py \
+    --eval_weights /home/gabeguo/uncertainty_mae/cifar100_linprobe_lower/checkpoint-85.pth
+
+python test_calibration.py \
+    --eval_weights /home/gabeguo/uncertainty_mae/cifar100_finetune_lower/checkpoint-99.pth \
+    --global_pool
+
+python test_calibration.py \
+    --eval_weights /home/gabeguo/uncertainty_mae/cifar100_finetune_upper/checkpoint-99.pth \
     --global_pool
