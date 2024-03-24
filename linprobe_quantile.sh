bf35d626992b29f8821dfd44976a09397e5b6806
Gabe Guo <gzg2104@columbia.edu>
2024-03-23 23:14:56 -0400

    Added linprobe for median

--- linprobe_quantile.sh
+++ linprobe_quantile.sh
@@ -13,3 +13,11 @@ python main_linprobe.py \
     --nb_classes 100 \
     --output_dir cifar100_linprobe_upper \
     --device cuda
+
+python main_linprobe.py \
+    --model vit_base_patch16 \
+    --finetune /home/gabeguo/uncertainty_mae/cifar100_train_median/checkpoint-399.pth \
+    --data_path /home/gabeguo/data/cifar-100-python \
+    --nb_classes 100 \
+    --output_dir cifar100_linprobe_median \
+    --device cuda
