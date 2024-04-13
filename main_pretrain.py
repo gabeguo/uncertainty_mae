649ac80c3270a553223eb981fe9027b1ecfca936
arnold-caleb <aa4870@columbia.edu>
2024-04-13 13:02:30 -0400

    modified distributed training (works with cifar100)

--- main_pretrain.py
+++ main_pretrain.py
@@ -101,9 +101,9 @@ def get_args_parser():
                         help='number of distributed processes')
     parser.add_argument('--local_rank', default=-1, type=int)
     parser.add_argument('--dist_on_itp', action='store_true')
-    # parser.add_argument('--dist_url', default='env://',
-    #                     help='url used to set up distributed training')
-    parser.add_argument('--distributed', action='store_true',
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
+    parser.add_argument('--distributed', default=True, action='store_true',
                         help='do distributed training or no distributed training')
                     
 
@@ -112,13 +112,10 @@ def get_args_parser():
 def main_distributed(rank, world_size, args):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
-    dist.init_process_group('nccl', rank=rank, world_size=world_size)
+    dist.init_process_group('nccl', init_method=args.dist_url, rank=rank, world_size=world_size)
 
-    print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
-    print("{}".format(args).replace(', ', ',\n'))
-
-    device = torch.device(f"cuda:{rank}")
-    # fix the seed for reproducibility
+    device = torch.device(f"cuda:{rank + 4}")  # Use the last 4 GPUs (4, 5, 6, 7)
+    torch.cuda.set_device(device)  # Set the current device for this process
     seed = args.seed + rank
 
     torch.manual_seed(seed)
@@ -126,54 +123,26 @@ def main_distributed(rank, world_size, args):
     torch.cuda.amp.autocast(enabled=False)
     cudnn.benchmark = True
 
-    # print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
-    # print("{}".format(args).replace(', ', ',\n'))
-
-    # TODO: better transform
-    # simple augmentation
     transform_train = transforms.Compose([
             transforms.RandomResizedCrop(args.input_size, scale=(0.2, 1.0), interpolation=3),  # 3 is bicubic
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
-    # transform_val = transforms.Compose([
-    #         transforms.Resize(256, interpolation=3),
-    #         transforms.CenterCrop(224),
-    #         transforms.ToTensor(),
-    #         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    #     ])
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    transform_val = transforms.Compose([
+            transforms.Resize(256, interpolation=3),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
     dataset_train = datasets.CIFAR100('../data', train=True, download=True, transform=transform_train) if args.dataset_name == 'cifar' else datasets.ImageNet(args.data_path, split="train", transform=transform_train, is_valid_file=lambda x: not x.split('/')[-1].startswith('.'))
     # dataset_val = datasets.CIFAR100('../data', train=False, download=True, transform=transform_val)
-
     
-    # print(dataset_train[0][0].shape)
-
-    # train_indices = [i for i in range(45000)]
-    # print(f'using only {len(train_indices)} indices')
-    # dataset_train = torch.utils.data.Subset(dataset_train, train_indices)
-
-    
-    print("Reached here")
     train_sampler = torch.utils.data.distributed.DistributedSampler(dataset_train, num_replicas=world_size, rank=rank, shuffle=True)
-    print("Train_sampler = %s" % str(train_sampler))
-    # if args.dist_eval:
-    #     if len(dataset_val) % num_tasks != 0:
-    #         print('Warning: Enabling distributed evaluation with an eval dataset not divisible by process number. '
-    #               'This will slightly alter validation results as extra duplicate entries are added to achieve '
-    #               'equal num of samples per-process.')
-    #     val_sampler = torch.utils.data.distributed.DistributedSampler(dataset_val, num_replicas=world_size, rank=rank, shuffle=True)
-    # else:
-    #     val_sampler = torch.utils.data.SequentialSampler(dataset_val)
 
+    data_loader_train = torch.utils.data.DataLoader(dataset_train, sampler=train_sampler, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, drop_last=True)
+    # data_loader_val = torch.utils.data.DataLoader(dataset_val, sampler=val_sampler, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, drop_last=False)
 
-    data_loader_train = torch.utils.data.DataLoader(
-        dataset_train, sampler=train_sampler, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, drop_last=True
-    )
-    # why no validation?
-    # data_loader_val = torch.utils.data.DataLoader(
-    #     dataset_val, sampler=val_sampler, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, drop_last=False
-    # )
+    
     if rank == 0 and args.log_dir is not None:
         os.makedirs(args.log_dir, exist_ok=True)
         log_writer = SummaryWriter(log_dir=args.log_dir)
@@ -200,31 +169,23 @@ def main_distributed(rank, world_size, args):
     model.to(device)
 
     model_without_ddp = model
-    # print("Model = %s" % str(model_without_ddp))
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-    print("Model = %s" % str(model_without_ddp))
-    print('number of params (M): %.2f' % (n_parameters / 1.e6))
+    # print("Model = %s" % str(model_without_ddp))
+    # print('number of params (M): %.2f' % (n_parameters / 1.e6))
 
-    eff_batch_size = args.batch_size * args.accum_iter * misc.get_world_size()
+    eff_batch_size = args.batch_size * args.accum_iter * world_size
     
     if args.lr is None:  # only base_lr is specified
         args.lr = args.blr * eff_batch_size / 256
 
-    # print("base lr: %.2e" % (args.lr * 256 / eff_batch_size))
-    # print("actual lr: %.2e" % args.lr)
-
-    # print("accumulate grad iterations: %d" % args.accum_iter)
-    # print("effective batch size: %d" % eff_batch_size)
-
-    
-    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[rank])#, find_unused_parameters=True)
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[rank + 4])
     model_without_ddp = model.module
     
     # following timm: set wd as 0 for bias and norm layers
     param_groups = optim_factory.add_weight_decay(model_without_ddp, args.weight_decay)
     optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
-    print(optimizer)
+    # print(optimizer)
     loss_scaler = NativeScaler()
 
     misc.load_model(args=args, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
@@ -236,10 +197,11 @@ def main_distributed(rank, world_size, args):
     else:
         wandb_name = f'mse'
 
-    model_name = wandb_name + datetime.now().strftime("%H:%M:%S")
+    model_name = wandb_name + datetime.datetime.now().strftime("%H:%M:%S")
     wandb.init(config=args, project='pretrain_mae', name=f"model_{model_name}")
     wandb.watch(model)
-    print(f"Start training for {args.epochs} epochs")
+    if rank == 0:
+        print(f"Start training for {args.epochs} epochs")
     start_time = time.time()
 
     for epoch in range(args.start_epoch, args.epochs):
@@ -248,7 +210,7 @@ def main_distributed(rank, world_size, args):
         train_stats = train_one_epoch(
             model, data_loader_train,
             optimizer, device, epoch, loss_scaler,
-            5, # Added this part
+            5,  # Added this part
             log_writer=log_writer,
             args=args
         )
@@ -271,10 +233,10 @@ def main_distributed(rank, world_size, args):
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
 
-    from thop import profile, clever_format # run pip3 install thop for this, i don't think it was in the original requirements.txt
+    from thop import profile, clever_format  # run pip3 install thop for this, i don't think it was in the original requirements.txt
 
     if rank == 0:
-        dummy_input = torch.rand(1, 3, 224, 224).to(device) # should give a good estimate of the model's complexity
+        dummy_input = torch.rand(1, 3, 224, 224).to(device)  # should give a good estimate of the model's complexity
         flops, params = profile(model, inputs=(dummy_input,))
         flops, params = clever_format([flops, params], "%.3f")
         print(f"FLOPs: {flops}, Params: {params}")
@@ -439,17 +401,14 @@ def main(args):
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
 
-
 if __name__ == '__main__':
     args = get_args_parser()
     args = args.parse_args()
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
-    
-    print("Distributed ? ", args.distributed)
+    print("Distributed?", args.distributed)
     if args.distributed:
-        world_size = torch.cuda.device_count()
-        print(world_size)
+        world_size = 4  # Use only the last 4 GPUs
         mp.spawn(main_distributed, args=(world_size, args), nprocs=world_size, join=True)
     else:
         main(args)
