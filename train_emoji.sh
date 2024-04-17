699e0daeda813c320cf032f21e7d05e67155dabe
Gabe Guo <gzg2104@columbia.edu>
2024-04-17 14:03:31 -0400

    batch size 64

--- train_emoji.sh
+++ train_emoji.sh
@@ -1,7 +1,7 @@
 python main_pretrain.py \
     --data_path /home/gabe/uncertainty_mae/dataset_generation/columbia_emoji \
     --dataset_name emoji \
-    --batch_size 128 \
+    --batch_size 64 \
     --accum_iter 1 \
     --output_dir emoji_train \
     --log_dir emoji_train \
