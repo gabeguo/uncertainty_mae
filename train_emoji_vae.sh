52bbc6c942d2f0a7fee1136d41ba5f98f678ec07
Gabe Guo <gzg2104@columbia.edu>
2024-04-23 19:53:38 -0400

    trying VAE now

---
+++ train_emoji_vae.sh
@@ -0,0 +1,13 @@
+python main_pretrain.py \
+    --data_path /home/gabe/uncertainty_mae/dataset_generation/columbia_emoji \
+    --dataset_name emoji \
+    --batch_size 64 \
+    --accum_iter 1 \
+    --output_dir emoji_train_couple \
+    --log_dir emoji_train_couple \
+    --model mae_vit_base_patch16 \
+    --epochs 20000 \
+    --log_freq 2000 \
+    --image_keywords couple \
+    --vae \
+    --kld_beta 1
