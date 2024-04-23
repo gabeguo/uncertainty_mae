39dec87b2bbee19bfec15539b77370ee3a2d2109
Gabe Guo <gzg2104@columbia.edu>
2024-04-23 18:23:37 -0400

    actually saves with correct name, and doesn't over-log

--- main_pretrain.py
+++ main_pretrain.py
@@ -113,7 +113,7 @@ def get_args_parser():
                     
 
     return parser
-
+"""
 def main_distributed(rank, world_size, args):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
@@ -254,7 +254,7 @@ def main_distributed(rank, world_size, args):
                         'flops': flops,
                         'runtime': total_time_str}
     wandb.log(log_stats)
-
+"""
 
 def main(args):
     
@@ -379,6 +379,8 @@ def main(args):
     else:
         wandb_name = f'mse'
 
+    wandb_name += f"_{args.dataset}_{'_'.join(args.image_keywords) if args.image_keywords is not None else ''}"
+
     wandb.init(config=args, project='pretrain_mae', name=f"model_{wandb_name}")
     wandb.watch(model)
     print(f"Start training for {args.epochs} epochs")
@@ -394,7 +396,7 @@ def main(args):
             log_writer=log_writer,
             args=args
         )
-        if args.output_dir and (epoch % 40 == 0 or epoch + 1 == args.epochs):
+        if args.output_dir and (epoch % args.log_interval == 0 or epoch + 1 == args.epochs):
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
                 loss_scaler=loss_scaler, epoch=epoch)
