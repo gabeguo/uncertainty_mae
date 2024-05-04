d1775415a7f64b4588e33f5fb1631f5204aa52b2
Gabe Guo <gzg2104@columbia.edu>
2024-05-04 15:04:25 -0400

    Bash script to train partial VAE

---
+++ train_emoji_partial_vae.sh
@@ -0,0 +1,16 @@
+python main_pretrain.py \
+    --data_path /home/gabe/uncertainty_mae/dataset_generation/columbia_emoji_full/train \
+    --dataset_name emoji \
+    --batch_size 64 \
+    --accum_iter 1 \
+    --output_dir emoji_train_partial_vae \
+    --log_dir emoji_train_partial_vae \
+    --model mae_vit_base_patch16 \
+    --epochs 20000 \
+    --log_freq 2000 \
+    --include_keywords kiss_ couple_with_heart_ people_holding_hands_ \
+    --include_any \
+    --vae \
+    --kld_beta 5 \
+    --mask_ratio 0.75 \
+    --partial_vae
