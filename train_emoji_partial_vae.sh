eb7504f90a5ad6946b478d0a5e275d07e2a61ca7
Gabe Guo <gzg2104@columbia.edu>
2024-05-16 00:14:18 -0400

    lowered both emoji and cifar blr to 5e-5

--- train_emoji_partial_vae.sh
+++ train_emoji_partial_vae.sh
@@ -2,10 +2,10 @@ python main_pretrain.py \
     --data_path /home/gzg2104/uncertainty_mae/dataset_generation/columbia_emoji/train \
     --dataset_name emoji \
     --batch_size 128 \
-    --blr 1e-4 \
+    --blr 5e-5 \
     --accum_iter 1 \
-    --output_dir /local/zemel/gzg2104/emoji_train_partial_vae_largeMAE \
-    --log_dir /local/zemel/gzg2104/emoji_train_partial_vae_largeMAE \
+    --output_dir /local/zemel/gzg2104/emoji_train_partial_vae_lr_5e-5 \
+    --log_dir /local/zemel/gzg2104/emoji_train_partial_vae_lr_5e-5 \
     --model mae_vit_large_patch16 \
     --epochs 4000 \
     --log_freq 200 \
