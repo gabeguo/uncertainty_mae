765c4f0714cb7ae97418e68346d84a6faf086066
Gabe Guo <gzg2104@columbia.edu>
2024-05-16 22:10:35 -0400

    trying batch size 128, blr 1e-3, warmup epochs 40, vit large, no mixed precision: see if this gets past 120

--- train_emoji_partial_vae.sh
+++ train_emoji_partial_vae.sh
@@ -1,13 +1,13 @@
 python main_pretrain.py \
     --data_path /home/gzg2104/uncertainty_mae/dataset_generation/columbia_emoji/train \
     --dataset_name emoji \
-    --batch_size 256 \
+    --batch_size 128 \
     --blr 1e-3 \
     --accum_iter 1 \
-    --output_dir /local/zemel/gzg2104/emoji_train_partial_vae_debug_nan \
-    --log_dir /local/zemel/gzg2104/emoji_train_partial_vae_debug_nan \
-    --model mae_vit_base_patch16 \
-    --warmup_epochs 20 \
+    --output_dir /local/zemel/gzg2104/emoji_train_partial_vae_no_mixed_precision \
+    --log_dir /local/zemel/gzg2104/emoji_train_partial_vae_no_mixed_precision \
+    --model mae_vit_large_patch16 \
+    --warmup_epochs 40 \
     --epochs 4000 \
     --log_freq 200 \
     --vae \
