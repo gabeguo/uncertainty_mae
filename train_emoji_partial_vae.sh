8009fe629b1beb14bb0843694ec65a04f5fa49dd
Gabe Guo <gzg2104@columbia.edu>
2024-05-17 10:55:14 -0400

    ensure that mixed precision option didn't break anything

--- train_emoji_partial_vae.sh
+++ train_emoji_partial_vae.sh
@@ -4,8 +4,8 @@ python main_pretrain.py \
     --batch_size 128 \
     --blr 5e-4 \
     --accum_iter 1 \
-    --output_dir /local/zemel/gzg2104/emoji_train_partial_vae_no_mixed_precision_eps_1e-4 \
-    --log_dir /local/zemel/gzg2104/emoji_train_partial_vae_no_mixed_precision_eps_1e-4 \
+    --output_dir /local/zemel/gzg2104/emoji_train_partial_vae_no_mixed_precision_eps_1e-4_REPLICATE \
+    --log_dir /local/zemel/gzg2104/emoji_train_partial_vae_no_mixed_precision_eps_1e-4_REPLICATE \
     --model mae_vit_large_patch16 \
     --warmup_epochs 40 \
     --epochs 4000 \
