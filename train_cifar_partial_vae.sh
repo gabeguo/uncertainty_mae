949cc4670789053b459bb8b1599191e793d58fe0
Gabe Guo <gzg2104@columbia.edu>
2024-05-19 12:30:44 -0400

    train cifar, 800 epochs, beta 5

--- train_cifar_partial_vae.sh
+++ train_cifar_partial_vae.sh
@@ -3,17 +3,17 @@ python main_pretrain.py \
     --batch_size 256 \
     --blr 1e-3 \
     --accum_iter 1 \
-    --output_dir /local/zemel/gzg2104/cifar_train_partial_vae_base_vit_batch_256_beta_10 \
-    --log_dir /local/zemel/gzg2104/cifar_train_partial_vae_base_vit_batch_256_beta_10 \
+    --output_dir /local/zemel/gzg2104/cifar_train_partial_vae_base_vit_batch_256_beta_5_epochs_800 \
+    --log_dir /local/zemel/gzg2104/cifar_train_partial_vae_base_vit_batch_256_beta_5_epochs_800 \
     --model mae_vit_base_patch16 \
     --warmup_epochs 40 \
-    --epochs 600 \
+    --epochs 800 \
     --log_freq 40 \
     --vae \
-    --kld_beta 10 \
+    --kld_beta 5 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
     --eps 1e-4 \
-    --weight_decay 0.1 \
+    --weight_decay 0.05 \
     --mixed_precision
