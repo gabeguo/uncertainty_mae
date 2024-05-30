9f9382bf52a104292bac000ddac84b6fbb970f53
Gabe Guo <gzg2104@columbia.edu>
2024-05-30 09:42:37 -0400

    beta 15

--- train_emoji_partial_vae.sh
+++ train_emoji_partial_vae.sh
@@ -4,14 +4,14 @@ python main_pretrain.py \
     --batch_size 256 \
     --blr 1e-3 \
     --accum_iter 1 \
-    --output_dir /local/zemel/gzg2104/_emoji_models/05_30_24_beta_10 \
-    --log_dir /local/zemel/gzg2104/_emoji_models/05_30_24_beta_10 \
+    --output_dir /local/zemel/gzg2104/_emoji_models/05_30_24_beta_15 \
+    --log_dir /local/zemel/gzg2104/_emoji_models/05_30_24_beta_15 \
     --model mae_vit_base_patch16 \
     --warmup_epochs 40 \
     --epochs 4000 \
     --log_freq 200 \
     --vae \
-    --kld_beta 10 \
+    --kld_beta 15 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
