c42c8bd3c16c88639e9afea7f22fbb165c63c5dc
Gabe Guo <gzg2104@columbia.edu>
2024-05-31 08:15:45 -0400

    beta 15, blr 2e-4

--- train_emoji_partial_vae.sh
+++ train_emoji_partial_vae.sh
@@ -2,17 +2,16 @@ python main_pretrain.py \
     --data_path /home/gzg2104/uncertainty_mae/dataset_generation/columbia_emoji/train \
     --dataset_name emoji \
     --batch_size 256 \
-    --blr 1e-3 \
+    --blr 2e-4 \
     --accum_iter 1 \
-    --output_dir /local/zemel/gzg2104/_emoji_models/05_30_24_beta_15_invisible_lr_0_1 \
-    --log_dir /local/zemel/gzg2104/_emoji_models/05_30_24_beta_15_invisible_lr_0_1 \
+    --output_dir /local/zemel/gzg2104/_emoji_models/05_30_24_beta_15_blr_2e-4 \
+    --log_dir /local/zemel/gzg2104/_emoji_models/05_30_24_beta_15_blr_2e-4 \
     --model mae_vit_base_patch16 \
     --warmup_epochs 40 \
     --epochs 4000 \
     --log_freq 200 \
     --vae \
     --kld_beta 15 \
-    --invisible_lr_scale 0.1 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
