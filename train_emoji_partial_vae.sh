a0d98616770dbe1e7febdb13178a4f14a6d9990a
Gabe Guo <gzg2104@columbia.edu>
2024-06-03 11:11:17 -0400

    Have batch size 128*2 for full VAE

--- train_emoji_partial_vae.sh
+++ train_emoji_partial_vae.sh
@@ -1,15 +1,19 @@
 python main_pretrain.py \
     --data_path /home/gzg2104/uncertainty_mae/dataset_generation/columbia_emoji/train \
     --dataset_name emoji \
-    --batch_size 256 \
+    --batch_size 128 \
     --blr 1e-3 \
-    --accum_iter 1 \
-    --output_dir /local/zemel/gzg2104/_emoji_models/06_03_24_vanilla_mae \
-    --log_dir /local/zemel/gzg2104/_emoji_models/06_03_24_vanilla_mae \
+    --accum_iter 2 \
+    --output_dir /local/zemel/gzg2104/_emoji_models/06_03_24_full_vae_beta_5 \
+    --log_dir /local/zemel/gzg2104/_emoji_models/06_03_24_full_vae_beta_5 \
     --model mae_vit_base_patch16 \
     --warmup_epochs 40 \
     --epochs 4000 \
     --log_freq 200 \
+    --vae \
+    --kld_beta 5 \
+    --mask_ratio 1 \
+    --partial_vae \
     --dropout_ratio 0 \
     --eps 1e-4 \
     --weight_decay 0.05 \
