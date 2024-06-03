13eebdb19d7921954c1e08373f85bb9ddb5367b7
Gabe Guo <gzg2104@columbia.edu>
2024-06-03 11:08:26 -0400

    vanilla MAE; weight decay 0.05

--- train_emoji_partial_vae.sh
+++ train_emoji_partial_vae.sh
@@ -4,16 +4,12 @@ python main_pretrain.py \
     --batch_size 256 \
     --blr 1e-3 \
     --accum_iter 1 \
-    --output_dir /local/zemel/gzg2104/_emoji_models/06_03_24_full_vae_beta_5 \
-    --log_dir /local/zemel/gzg2104/_emoji_models/06_03_24_full_vae_beta_5 \
+    --output_dir /local/zemel/gzg2104/_emoji_models/06_03_24_vanilla_mae \
+    --log_dir /local/zemel/gzg2104/_emoji_models/06_03_24_vanilla_mae \
     --model mae_vit_base_patch16 \
     --warmup_epochs 40 \
     --epochs 4000 \
     --log_freq 200 \
-    --vae \
-    --kld_beta 5 \
-    --mask_ratio 1 \
-    --partial_vae \
     --dropout_ratio 0 \
     --eps 1e-4 \
     --weight_decay 0.05 \
