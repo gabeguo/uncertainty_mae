a01964677f7c183954133096aab41207b955246a
Gabe Guo <gzg2104@columbia.edu>
2024-06-06 16:47:50 -0400

    Low regularization version, alter eps to 1e-6

--- train_emoji_partial_vae.sh
+++ train_emoji_partial_vae.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_emoji_models/06_05_24_lessReg_decay_0_01_lrScale_0_2
+output_path=/local/zemel/gzg2104/_emoji_models/06_06_24_eps_1e-7
 python main_pretrain.py \
     --data_path /home/gzg2104/uncertainty_mae/dataset_generation/columbia_emoji/train \
     --dataset_name emoji \
@@ -13,10 +13,10 @@ python main_pretrain.py \
     --log_freq 200 \
     --vae \
     --kld_beta 20 \
-    --invisible_lr_scale 0.2 \
+    --invisible_lr_scale 0.1 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
-    --eps 1e-6 \
-    --weight_decay 0.01 \
+    --eps 1e-7 \
+    --weight_decay 0.025 \
     --mixed_precision
