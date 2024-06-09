7c4d3471c831a6b9f41621d3dd7f0b07a2123839
Gabe Guo <gzg2104@columbia.edu>
2024-06-09 17:44:21 -0400

    higher mask ratio for cifar model: 0.8

--- train_cifar_partial_vae.sh
+++ train_cifar_partial_vae.sh
@@ -9,11 +9,11 @@ python main_pretrain.py \
     --model mae_vit_base_patch16 \
     --warmup_epochs 40 \
     --epochs 800 \
-    --log_freq 200 \
+    --log_freq 50 \
     --vae \
     --kld_beta 20 \
     --invisible_lr_scale 0.1 \
-    --mask_ratio 0.75 \
+    --mask_ratio 0.8 \
     --partial_vae \
     --dropout_ratio 0 \
     --eps 1e-6 \
