981aa67c86088ddd55ab0aee98ee6c792f4a58e4
Gabe Guo <gzg2104@columbia.edu>
2024-06-12 14:28:27 -0400

    Train emoji for 3000 epochs

--- train_emoji_partial_vae.sh
+++ train_emoji_partial_vae.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_emoji_models/06_12_24_batchSize_384
+output_path=/local/zemel/gzg2104/_emoji_models/06_12_24_batchSize_384_epochs_3000
 python main_pretrain.py \
     --data_path /home/gzg2104/uncertainty_mae/dataset_generation/columbia_emoji/train \
     --dataset_name emoji \
@@ -9,7 +9,7 @@ python main_pretrain.py \
     --log_dir $output_path \
     --model mae_vit_base_patch16 \
     --warmup_epochs 100 \
-    --epochs 2500 \
+    --epochs 3000 \
     --log_freq 200 \
     --vae \
     --kld_beta 20 \
