51bbe6ca2daaf6f02be21e0f61df2e6995fa5898
Gabe Guo <gzg2104@columbia.edu>
2024-06-14 14:25:34 -0400

    Script to train imagenet sketch

---
+++ train_imagenet_sketch.sh
@@ -0,0 +1,22 @@
+output_path=/local/zemel/gzg2104/_sketch_models/06_14_24_initialTry
+python main_pretrain.py \
+    --dataset_name imagenet_sketch \
+    --batch_size 256 \
+    --blr 1e-3 \
+    --accum_iter 1 \
+    --output_dir $output_path \
+    --log_dir $output_path \
+    --model mae_vit_base_patch16 \
+    --warmup_epochs 40 \
+    --epochs 800 \
+    --log_freq 200 \
+    --vae \
+    --kld_beta 20 \
+    --invisible_lr_scale 0.1 \
+    --mask_ratio 0.75 \
+    --partial_vae \
+    --dropout_ratio 0 \
+    --eps 1e-6 \
+    --weight_decay 0.025 \
+    --mixed_precision \
+    --wandb_project imagenet_sketch_pretrain
