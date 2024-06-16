c52fb1421ab08844760c183fbec278c7dfc4dc5a
Gabe Guo <gzg2104@columbia.edu>
2024-06-16 19:06:05 -0400

    unfreeze after 100 epochs

--- train_cifar_partial_vae.sh
+++ train_cifar_partial_vae.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_cifar_models/06_16_24_sharedEncoder_pretrained
+output_path=/local/zemel/gzg2104/_cifar_models/06_16_24_sharedEncoder_unfreezeAfter100
 python main_pretrain.py \
     --dataset_name cifar \
     --batch_size 256 \
@@ -19,6 +19,6 @@ python main_pretrain.py \
     --weight_decay 0.025 \
     --mixed_precision \
     --pretrained_weights /home/gzg2104/uncertainty_mae/pretrained_models/mae_visualize_vit_base.pth \
-    --frozen_backbone_epochs 200 \
+    --frozen_backbone_epochs 100 \
     --same_encoder \
     --wandb_project cifar_pretrain
