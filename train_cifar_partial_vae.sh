9a600d842c7dd95ecc84802990bdbeac0c2d1226
Gabe Guo <gzg2104@columbia.edu>
2024-06-16 19:24:39 -0400

    2 VAE blocks

--- train_cifar_partial_vae.sh
+++ train_cifar_partial_vae.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_cifar_models/06_16_24_sharedEncoder_unfreezeAfter100_beta15
+output_path=/local/zemel/gzg2104/_cifar_models/06_16_24_sharedEncoder_vaeBlocks2
 python main_pretrain.py \
     --dataset_name cifar \
     --batch_size 256 \
@@ -19,6 +19,7 @@ python main_pretrain.py \
     --weight_decay 0.025 \
     --mixed_precision \
     --pretrained_weights /home/gzg2104/uncertainty_mae/pretrained_models/mae_visualize_vit_base.pth \
-    --frozen_backbone_epochs 100 \
+    --frozen_backbone_epochs 800 \
     --same_encoder \
+    --num_vae_blocks 2 \
     --wandb_project cifar_pretrain
