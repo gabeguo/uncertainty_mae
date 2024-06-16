35c3830f17f625bc6308240bf454684c00047227
Gabe Guo <gzg2104@columbia.edu>
2024-06-16 16:20:58 -0400

    train sketch with common encoder and pre-trained weights

--- train_imagenet_sketch.sh
+++ train_imagenet_sketch.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_sketch_models/06_14_24_initialTry
+output_path=/local/zemel/gzg2104/_sketch_models/06_16_24_sharedEncoder_pretrained
 python main_pretrain.py \
     --dataset_name imagenet_sketch \
     --batch_size 256 \
@@ -9,14 +9,16 @@ python main_pretrain.py \
     --model mae_vit_base_patch16 \
     --warmup_epochs 40 \
     --epochs 800 \
-    --log_freq 40 \
+    --log_freq 50 \
     --vae \
-    --kld_beta 20 \
-    --invisible_lr_scale 0.1 \
+    --kld_beta 5 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
     --eps 1e-6 \
     --weight_decay 0.025 \
     --mixed_precision \
+    --pretrained_weights /home/gzg2104/uncertainty_mae/pretrained_models/mae_visualize_vit_base.pth \
+    --frozen_backbone_epochs 500 \
+    --same_encoder \
     --wandb_project imagenet_sketch_pretrain
