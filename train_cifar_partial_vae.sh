b5d387a72682ba2e77e7f21141e7e48add1a848a
Gabe Guo <gzg2104@columbia.edu>
2024-06-17 23:14:00 -0400

    end-to-end finetune with beta 15

--- train_cifar_partial_vae.sh
+++ train_cifar_partial_vae.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_cifar_models/06_17_24_lr_1e-3/end_to_end_finetune_beta10
+output_path=/local/zemel/gzg2104/_cifar_models/06_17_24_lr_1e-3/end_to_end_finetune_beta15
 python main_pretrain.py \
     --dataset_name cifar \
     --batch_size 384 \
@@ -11,7 +11,7 @@ python main_pretrain.py \
     --epochs 300 \
     --log_freq 50 \
     --vae \
-    --kld_beta 10 \
+    --kld_beta 15 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
