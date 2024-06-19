c6b143fdb1364fbc3ad9e153af737d48dcdc7c82
Gabe Guo <gzg2104@columbia.edu>
2024-06-19 14:20:31 -0400

    Block mask prob of 0.25, invisible lr scale of 0.5

--- train_cifar_partial_vae.sh
+++ train_cifar_partial_vae.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_cifar_models/06_19_24_partialVAE_retry/finetune_heads/scale_0_5_noBlock
+output_path=/local/zemel/gzg2104/_cifar_models/06_19_24_partialVAE_retry/finetune_heads/scale_0_5_block_0_25
 num_epochs=400
 python main_pretrain.py \
     --dataset_name cifar \
@@ -21,7 +21,7 @@ python main_pretrain.py \
     --weight_decay 0.025 \
     --mixed_precision \
     --num_vae_blocks 1 \
-    --block_mask_prob 0 \
+    --block_mask_prob 0.25 \
     --wandb_project cifar_pretrain \
     --pretrained_weights /home/gzg2104/uncertainty_mae/pretrained_models/mae_visualize_vit_base.pth \
     --frozen_backbone_epochs $num_epochs \
