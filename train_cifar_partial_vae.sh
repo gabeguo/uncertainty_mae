9cbd5dd76db92cfbf83f1f8873294101bc6f1f69
Gabe Guo <gzg2104@columbia.edu>
2024-06-19 14:23:54 -0400

    Invisible lr scale 0.1, block mask ratio 0.25

--- train_cifar_partial_vae.sh
+++ train_cifar_partial_vae.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_cifar_models/06_19_24_partialVAE_retry/finetune_heads/scale_0_5_block_0_25
+output_path=/local/zemel/gzg2104/_cifar_models/06_19_24_partialVAE_retry/finetune_heads/scale_0_1_block_0_25
 num_epochs=400
 python main_pretrain.py \
     --dataset_name cifar \
@@ -13,7 +13,7 @@ python main_pretrain.py \
     --log_freq 50 \
     --vae \
     --kld_beta 5 \
-    --invisible_lr_scale 0.5 \
+    --invisible_lr_scale 0.1 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
