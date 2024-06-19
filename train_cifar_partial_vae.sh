ef326d8a55f2d16a1a0efe2ed18186600c89537b
Gabe Guo <gzg2104@columbia.edu>
2024-06-19 14:08:20 -0400

    Retrying partial VAE approach, but with pre-loaded weights and zero conv

--- train_cifar_partial_vae.sh
+++ train_cifar_partial_vae.sh
@@ -1,8 +1,8 @@
-output_path=/local/zemel/gzg2104/_cifar_models/06_18_24_blockMask/end_to_end_finetune_400epochs
+output_path=/local/zemel/gzg2104/_cifar_models/06_19_24_partialVAE_retry/finetune_heads/
 num_epochs=400
 python main_pretrain.py \
     --dataset_name cifar \
-    --batch_size 384 \
+    --batch_size 512 \
     --blr 1e-4 \
     --accum_iter 1 \
     --output_dir $output_path \
@@ -13,16 +13,15 @@ python main_pretrain.py \
     --log_freq 50 \
     --vae \
     --kld_beta 5 \
+    --invisible_lr_scale 0.1 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
     --eps 1e-8 \
     --weight_decay 0.025 \
     --mixed_precision \
-    --pretrained_weights /local/zemel/gzg2104/_cifar_models/06_18_24_blockMask/finetune_vaeHeads_and_decoder_400epochs/checkpoint-399.pth \
-    --frozen_backbone_epochs 0 \
-    --same_encoder \
     --num_vae_blocks 1 \
-    --block_mask_prob 0.5 \
+    --block_mask_prob 0 \
     --wandb_project cifar_pretrain \
-    --end_to_end_finetune
+    --pretrained_weights /home/gzg2104/uncertainty_mae/pretrained_models/mae_visualize_vit_base.pth \
+    --frozen_backbone_epochs $num_epochs \
