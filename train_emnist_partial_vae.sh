10dfd2bf564ca427fe52ded34478cbedb3067b6d
Gabe Guo <gzg2104@columbia.edu>
2024-06-20 18:40:21 -0400

    Try EMNIST

---
+++ train_emnist_partial_vae.sh
@@ -0,0 +1,22 @@
+output_path=/local/zemel/gzg2104/_emnist_models/initial_try_06_20_24
+python main_pretrain.py \
+    --dataset_name emnist \
+    --batch_size 384 \
+    --blr 1e-3 \
+    --accum_iter 1 \
+    --output_dir $output_path \
+    --log_dir $output_path \
+    --model mae_vit_base_patch16 \
+    --warmup_epochs 40 \
+    --epochs 800 \
+    --log_freq 40 \
+    --vae \
+    --kld_beta 25 \
+    --invisible_lr_scale 0.1 \
+    --mask_ratio 0.75 \
+    --partial_vae \
+    --dropout_ratio 0 \
+    --eps 1e-8 \
+    --weight_decay 0.025 \
+    --mixed_precision \
+    --wandb_project emnist_pretrain
