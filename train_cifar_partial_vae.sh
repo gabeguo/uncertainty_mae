1435e1446a979b8eaa7128d7fcbdcb6be4de4079
Gabe Guo <gzg2104@columbia.edu>
2024-06-20 14:38:20 -0400

    finetune with lr 5e-5

--- train_cifar_partial_vae.sh
+++ train_cifar_partial_vae.sh
@@ -1,9 +1,9 @@
-output_path=/local/zemel/gzg2104/_cifar_models/06_20_24_partialVAE_retry/finetune_heads/decoderTrainOnly
+output_path=/local/zemel/gzg2104/_cifar_models/06_19_24_partialVAE_retry/end_to_end_finetune/lr_5e-5
 num_epochs=400
 python main_pretrain.py \
     --dataset_name cifar \
-    --batch_size 512 \
-    --blr 5e-4 \
+    --batch_size 384 \
+    --blr 5e-5 \
     --accum_iter 1 \
     --output_dir $output_path \
     --log_dir $output_path \
@@ -13,15 +13,16 @@ python main_pretrain.py \
     --log_freq 50 \
     --vae \
     --kld_beta 10 \
-    --invisible_lr_scale 0 \
+    --invisible_lr_scale 0.1 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
     --eps 1e-8 \
     --weight_decay 0.025 \
     --mixed_precision \
-    --num_vae_blocks 2 \
+    --num_vae_blocks 1 \
     --block_mask_prob 0 \
     --wandb_project cifar_pretrain \
-    --pretrained_weights /home/gzg2104/uncertainty_mae/pretrained_models/mae_visualize_vit_base.pth \
+    --pretrained_weights /local/zemel/gzg2104/_cifar_models/06_19_24_partialVAE_retry/finetune_heads/zeroConvMean_fasterLr/checkpoint-399.pth \
     --frozen_backbone_epochs $num_epochs \
+    --end_to_end_finetune
