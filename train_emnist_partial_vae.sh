a2052972ebc19ca490555677b8d0429b5c3fdd36
Gabe Guo <gzg2104@columbia.edu>
2024-06-21 14:07:01 -0400

    version that disables zero conv for emnist

--- train_emnist_partial_vae.sh
+++ train_emnist_partial_vae.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_emnist_models/06_21_24_eps_1e-4
+output_path=/local/zemel/gzg2104/_emnist_models/06_21_24_noZeroConv
 python main_pretrain.py \
     --dataset_name emnist \
     --batch_size 384 \
@@ -16,7 +16,8 @@ python main_pretrain.py \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
-    --eps 1e-4 \
+    --eps 1e-8 \
     --weight_decay 0.025 \
     --mixed_precision \
-    --wandb_project emnist_pretrain
+    --wandb_project emnist_pretrain \
+    --disable_zero_conv
