b49d27863a6723c414ef017fda77cd5e7a236d29
Gabe Guo <gzg2104@columbia.edu>
2024-06-23 21:46:18 -0400

    Beta 5, eps 1d-6, blr 1e-4

--- train_emnist_partial_vae.sh
+++ train_emnist_partial_vae.sh
@@ -1,8 +1,8 @@
-output_path=/local/zemel/gzg2104/_emnist_models/06_22_24/common_encoder/beta1_blockMask_0_5
+output_path=/local/zemel/gzg2104/_emnist_models/06_23_24/common_encoder/beta5_eps_1e-6_blr_1e-4
 python main_pretrain.py \
     --dataset_name emnist \
     --batch_size 384 \
-    --blr 1e-3 \
+    --blr 1e-4 \
     --accum_iter 1 \
     --output_dir $output_path \
     --log_dir $output_path \
@@ -11,14 +11,14 @@ python main_pretrain.py \
     --epochs 800 \
     --log_freq 40 \
     --vae \
-    --kld_beta 1 \
+    --kld_beta 5 \
     --invisible_lr_scale 0.1 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
-    --eps 1e-8 \
+    --eps 1e-6 \
     --weight_decay 0.05 \
     --mixed_precision \
     --same_encoder \
-    --block_mask_prob 0.5 \
+    --block_mask_prob 0 \
     --wandb_project emnist_pretrain
