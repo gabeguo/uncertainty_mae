79a53aeb71097c86c366b984d174b151085593e4
Gabe Guo <gzg2104@columbia.edu>
2024-06-25 17:15:45 -0400

    lr 2e-4 for vanilla cifar

--- train_cifar_partial_vae.sh
+++ train_cifar_partial_vae.sh
@@ -1,8 +1,8 @@
-output_path=/local/zemel/gzg2104/_cifar_models/06_25_24_commonEncoder_yesZeroConv
+output_path=/local/zemel/gzg2104/_cifar_models/06_25_24_vanilla_lr2e-4
 python main_pretrain.py \
     --dataset_name cifar \
     --batch_size 384 \
-    --blr 5e-4 \
+    --blr 2e-4 \
     --accum_iter 1 \
     --output_dir $output_path \
     --log_dir $output_path \
@@ -10,14 +10,11 @@ python main_pretrain.py \
     --warmup_epochs 40 \
     --epochs 800 \
     --log_freq 50 \
-    --vae \
     --kld_beta 25 \
-    --invisible_lr_scale 0.1 \
     --mask_ratio 0.75 \
-    --partial_vae \
     --dropout_ratio 0 \
     --eps 1e-6 \
     --weight_decay 0.025 \
     --mixed_precision \
     --wandb_project cifar_pretrain \
-    --same_encoder
+    --disable_zero_conv
