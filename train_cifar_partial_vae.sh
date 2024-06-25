80d3b4a95dba7c44c77da2f715508dc73ffbe410
Gabe Guo <gzg2104@columbia.edu>
2024-06-25 16:53:17 -0400

    vanilla cifar

--- train_cifar_partial_vae.sh
+++ train_cifar_partial_vae.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_cifar_models/fromScratch_06_21_24_zeroConv_eps_1e-4
+output_path=/local/zemel/gzg2104/_cifar_models/06_25_24_vanilla
 python main_pretrain.py \
     --dataset_name cifar \
     --batch_size 384 \
@@ -10,13 +10,12 @@ python main_pretrain.py \
     --warmup_epochs 40 \
     --epochs 800 \
     --log_freq 50 \
-    --vae \
     --kld_beta 25 \
     --invisible_lr_scale 0.1 \
     --mask_ratio 0.75 \
-    --partial_vae \
     --dropout_ratio 0 \
-    --eps 1e-4 \
+    --eps 1e-6 \
     --weight_decay 0.025 \
     --mixed_precision \
-    --wandb_project cifar_pretrain
+    --wandb_project cifar_pretrain \
+    --disable_zero_conv
