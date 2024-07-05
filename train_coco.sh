38a3061b460eb762d62bb624399bada8c597a889
Gabe Guo <gzg2104@columbia.edu>
2024-07-05 18:43:08 -0400

    beta 50, blr 1.5e-4

--- train_coco.sh
+++ train_coco.sh
@@ -1,8 +1,8 @@
-output_path=/local/zemel/gzg2104/_coco_models/07_03_24/eps1e-8
+output_path=/local/zemel/gzg2104/_coco_models/07_05_24/beta50
 python main_pretrain.py \
     --dataset_name coco \
     --batch_size 384 \
-    --blr 1e-3 \
+    --blr 1.5e-4 \
     --accum_iter 1 \
     --output_dir $output_path \
     --log_dir $output_path \
@@ -11,12 +11,12 @@ python main_pretrain.py \
     --epochs 800 \
     --log_freq 40 \
     --vae \
-    --kld_beta 25 \
+    --kld_beta 50 \
     --invisible_lr_scale 0.1 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
-    --eps 1e-8 \
+    --eps 1e-6 \
     --weight_decay 0.025 \
     --mixed_precision \
     --wandb_project coco_pretrain \
