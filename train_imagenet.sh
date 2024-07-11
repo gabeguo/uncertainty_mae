edd85a9be1a467ef93b28658f22710a94ea71fc5
Gabe Guo <gzg2104@columbia.edu>
2024-07-11 10:04:35 -0400

    reduced KLD beta to 5, invisible lr scale to 0.025, lr to 1e-4

--- train_imagenet.sh
+++ train_imagenet.sh
@@ -1,19 +1,19 @@
-output_path=/local/zemel/gzg2104/_imagenet_models/07_10_24/beta10
+output_path=/local/zemel/gzg2104/_imagenet_models/07_11_24/beta5_blr1e-4_invScale025
 python main_pretrain.py \
     --dataset_name imagenet \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --batch_size 384 \
-    --blr 1.5e-4 \
+    --blr 1e-4 \
     --accum_iter 1 \
     --output_dir $output_path \
     --log_dir $output_path \
     --model mae_vit_base_patch16 \
     --warmup_epochs 40 \
     --epochs 800 \
-    --log_freq 40 \
+    --log_freq 25 \
     --vae \
-    --kld_beta 10 \
-    --invisible_lr_scale 0.1 \
+    --kld_beta 5 \
+    --invisible_lr_scale 0.025 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
