7c06d5f0ccdcc65c7b27eefbe218746264f6b0ad
Gabriel Z. Guo <gzg2104@mlogin002.cm.cluster>
2024-07-13 20:04:17 -0400

    try to fix wandb logging

--- slurm_main_pretrain.py
+++ slurm_main_pretrain.py
@@ -385,7 +385,7 @@ def main(args):
 
         log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
                         'epoch': epoch,}
-        if args.local_rank == 0:
+        if args.gpu == 0:
             wandb.log(log_stats, step=epoch)
 
         if args.output_dir and misc.is_main_process():
