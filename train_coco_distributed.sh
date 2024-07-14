ff8a6998604ca9bb975a2252ed1576ce3ce964bb
Gabriel Z. Guo <gzg2104@mlogin002.cm.cluster>
2024-07-13 20:06:12 -0400

    retry coco training distributed with more conservative hyperparam settings

--- train_coco_distributed.sh
+++ train_coco_distributed.sh
@@ -11,18 +11,18 @@ WORKDIR=$(pwd)
 output_path=/burg/zgroup/users/gzg2104/_coco_models/07_11_24/initial_try
 JOB_DIR=$(pwd)
 python $WORKDIR/submitit_pretrain.py \
-    --ngpus 3 \
-    --nodes 1 \
+    --ngpus 8 \
+    --nodes 3 \
     --timeout 720 \
     --job_dir $JOB_DIR \
-    --partition zgroup \
+    --partition short \
     --account zgroup \
-    --job_name dummy_mae \
+    --job_name mae_retry \
     --output mae.out \
     --error mae.err \
     --dataset_name coco \
     --batch_size 128 \
-    --blr 1e-3 \
+    --blr 1.5e-4 \
     --accum_iter 1 \
     --output_dir $output_path \
     --log_dir $output_path \
@@ -31,13 +31,13 @@ python $WORKDIR/submitit_pretrain.py \
     --epochs 800 \
     --log_freq 40 \
     --vae \
-    --kld_beta 25 \
-    --invisible_lr_scale 0.1 \
+    --kld_beta 10 \
+    --invisible_lr_scale 0.025 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
-    --eps 1e-8 \
-    --weight_decay 0.025 \
+    --eps 1e-6 \
+    --weight_decay 0.05 \
     --mixed_precision \
     --wandb_project coco_pretrain \
     --wandb_name initialTrySlurm \
