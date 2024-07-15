7c4c3acd71b57b7c655ef80a5eb669b726df8794
Gabe Guo <gzg2104@columbia.edu>
2024-07-15 10:45:56 -0400

    linprobe checkpoint 400

--- linprobe_uncertainty.sh
+++ linprobe_uncertainty.sh
@@ -1,10 +1,10 @@
 python main_linprobe.py \
     --model vit_base_patch16 \
-    --finetune /local/zemel/gzg2104/_coco_models/07_03_24/beta25_blr1_5e-4/checkpoint-720.pth \
+    --finetune /local/zemel/gzg2104/_imagenet_models/07_11_24/beta5_blr1e-4_invScale025/checkpoint-400.pth \
     --dataset_name imagenet \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --nb_classes 1000 \
-    --output_dir /local/zemel/gzg2104/_coco_models/07_03_24/beta25_blr1_5e-4/finetune \
+    --output_dir /local/zemel/gzg2104/_imagenet_models/07_11_24/beta5_blr1e-4_invScale025/finetune-400 \
     --batch_size 4096 \
     --log_dir /local/zemel/gzg2104/logs \
     --wandb_project linprobe_imagenet \
