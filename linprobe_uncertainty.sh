349bfd051be757c7f971cd9049ae7f1bb0737c99
Gabe Guo <gzg2104@columbia.edu>
2024-07-16 09:41:51 -0400

    try linprobing epoch 500

--- linprobe_uncertainty.sh
+++ linprobe_uncertainty.sh
@@ -1,13 +1,13 @@
 python main_linprobe.py \
     --model vit_base_patch16 \
-    --finetune /local/zemel/gzg2104/_imagenet_models/07_11_24/beta5_blr1e-4_invScale025/checkpoint-200.pth \
+    --finetune /local/zemel/gzg2104/_imagenet_models/07_11_24/beta5_blr1e-4_invScale025/checkpoint-500.pth \
     --dataset_name imagenet \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --nb_classes 1000 \
-    --output_dir /local/zemel/gzg2104/_imagenet_models/07_11_24/beta5_blr1e-4_invScale025/finetune-200 \
+    --output_dir /local/zemel/gzg2104/_imagenet_models/07_11_24/beta5_blr1e-4_invScale025/finetune-500 \
     --batch_size 4096 \
     --log_dir /local/zemel/gzg2104/logs \
     --wandb_project linprobe_imagenet \
     --device cuda \
-    --master_port 12356
+    --master_port 12357
 
