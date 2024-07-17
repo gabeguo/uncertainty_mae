321ddf61147c7f8c15a5629ae1a47a36fbbbdfee
Gabe Guo <gzg2104@columbia.edu>
2024-07-17 11:28:29 -0400

    compare linprobes on cifar

--- linprobe_uncertainty.sh
+++ linprobe_uncertainty.sh
@@ -1,13 +1,13 @@
 python main_linprobe.py \
     --model vit_base_patch16 \
-    --finetune /local/zemel/gzg2104/_imagenet_models/07_11_24/beta5_blr1e-4_invScale025/checkpoint-500.pth \
-    --dataset_name imagenet \
+    --finetune /local/zemel/gzg2104/_imagenet_models/07_15_24/beta30_scale0_01_epochs400/checkpoint-175.pth \
+    --dataset_name cifar \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
-    --nb_classes 1000 \
-    --output_dir /local/zemel/gzg2104/_imagenet_models/07_11_24/beta5_blr1e-4_invScale025/finetune-500 \
-    --batch_size 4096 \
+    --nb_classes 100 \
+    --output_dir /local/zemel/gzg2104/_imagenet_models/07_15_24/beta30_scale0_01_epochs400/finetune-175 \
+    --batch_size 1024 \
     --log_dir /local/zemel/gzg2104/logs \
-    --wandb_project linprobe_imagenet \
+    --wandb_project linprobe_cifar \
     --device cuda \
-    --master_port 12357
+    --master_port 12356
 
