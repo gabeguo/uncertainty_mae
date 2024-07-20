4cd594a93f4dc3174a6cfb5fb881ff6bd5007f93
Gabe Guo <gzg2104@columbia.edu>
2024-07-20 10:20:23 -0400

    linprobe default mask

--- linprobe_uncertainty.sh
+++ linprobe_uncertainty.sh
@@ -1,13 +1,13 @@
 python main_linprobe.py \
     --model vit_base_patch16 \
-    --finetune /local/zemel/gzg2104/_coco_models/07_19_24/beta50/checkpoint-399.pth \
+    --finetune /local/zemel/gzg2104/_coco_models/07_19_24/beta30_addDefaultMask/checkpoint-399.pth \
     --dataset_name cifar \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --nb_classes 100 \
-    --output_dir /local/zemel/gzg2104/_coco_models/07_19_24/beta50/linprobe \
+    --output_dir /local/zemel/gzg2104/_coco_models/07_19_24/beta30_addDefaultMask/linprobe \
     --batch_size 1024 \
     --log_dir /local/zemel/gzg2104/logs \
     --wandb_project linprobe_cifar \
     --device cuda \
-    --master_port 12356
+    --master_port 12357
 
