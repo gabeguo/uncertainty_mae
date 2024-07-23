85a1c5d6898055a9dc02a81e7409ab8f818ee41a
Gabe Guo <gzg2104@columbia.edu>
2024-07-22 22:13:27 -0400

    train baseline, no partial VAE

--- train_coco.sh
+++ train_coco.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_coco_models/07_22_24/RETRY_beta30_addDefaultMask_prior0_01
+output_path=/local/zemel/gzg2104/_coco_models/07_22_24/baseline
 python main_pretrain.py \
     --dataset_name coco \
     --batch_size 384 \
@@ -10,18 +10,12 @@ python main_pretrain.py \
     --warmup_epochs 40 \
     --epochs 400 \
     --log_freq 25 \
-    --vae \
-    --kld_beta 30 \
-    --invisible_lr_scale 0.01 \
     --mask_ratio 0.75 \
-    --partial_vae \
     --dropout_ratio 0 \
     --eps 1e-6 \
     --weight_decay 0.05 \
     --mixed_precision \
     --wandb_project RETRY_coco_head_to_head \
     --disable_zero_conv \
-    --master_port 12355 \
-    --object_mask \
-    --add_default_mask \
-    --var 0.01
+    --master_port 12356 \
+    --object_mask
