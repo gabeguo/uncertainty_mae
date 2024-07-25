6f9535202481f22d4961a0254db4397b9d4aacef
Gabe Guo <gzg2104@columbia.edu>
2024-07-25 18:53:01 -0400

    linprobe baseline, eps 1e-8

--- linprobe_uncertainty.sh
+++ linprobe_uncertainty.sh
@@ -1,13 +1,13 @@
 python main_linprobe.py \
     --model vit_base_patch16 \
-    --finetune /local/zemel/gzg2104/_coco_models/07_24_24/beta30_addDefault_eps1e-8_scale0_025/checkpoint-350.pth \
+    --finetune /local/zemel/gzg2104/_coco_models/07_24_24/baseline_eps1e-8/checkpoint-350.pth \
     --dataset_name cifar \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --nb_classes 100 \
-    --output_dir /local/zemel/gzg2104/_coco_models/07_24_24/beta30_addDefault_eps1e-8_scale0_025/linprobe \
+    --output_dir /local/zemel/gzg2104/_coco_models/07_24_24/baseline_eps1e-8/linprobe \
     --batch_size 1024 \
     --log_dir /local/zemel/gzg2104/logs \
     --wandb_project linprobe_cifar \
     --device cuda \
-    --master_port 12357
+    --master_port 12358
 
