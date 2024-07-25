1c58357318ab6943e59e303999663929d78ddee2
Gabe Guo <gzg2104@columbia.edu>
2024-07-25 18:50:49 -0400

    linprobe scale 0.025

--- linprobe_uncertainty.sh
+++ linprobe_uncertainty.sh
@@ -1,10 +1,10 @@
 python main_linprobe.py \
     --model vit_base_patch16 \
-    --finetune /local/zemel/gzg2104/_coco_models/07_22_24/baseline/checkpoint-399.pth \
+    --finetune /local/zemel/gzg2104/_coco_models/07_24_24/beta30_addDefault_eps1e-8_scale0_025/checkpoint-350.pth \
     --dataset_name cifar \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --nb_classes 100 \
-    --output_dir /local/zemel/gzg2104/_coco_models/07_22_24/baseline/linprobe \
+    --output_dir /local/zemel/gzg2104/_coco_models/07_24_24/beta30_addDefault_eps1e-8_scale0_025/linprobe \
     --batch_size 1024 \
     --log_dir /local/zemel/gzg2104/logs \
     --wandb_project linprobe_cifar \
