99b6afae8df7d28d648782cea4d6c53cc9f6510a
Gabriel Z. Guo <gzg2104@mlogin002.cm.cluster>
2024-07-30 10:16:25 -0400

    add distributed training imagenet

---
+++ train_imagenet_distributed.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+WORKDIR=$(pwd)
+output_path=/burg/zgroup/users/gzg2104/_imagenet_models/07_28_24/initialTry
+JOB_DIR=$(pwd)
+python $WORKDIR/submitit_pretrain.py \
+    --ngpus 8 \
+    --nodes 2 \
+    --timeout 720 \
+    --job_dir $JOB_DIR \
+    --partition short \
+    --account zgroup \
+    --job_name imagenet_try \
+    --output mae.out \
+    --error mae.err \
+    --dataset_name imagenet \
+    --data_path /burg/zgroup/users/gzg2104/data/imagenet/train \
+    --batch_size 128 \
+    --blr 1.5e-4 \
+    --accum_iter 1 \
+    --output_dir $output_path \
+    --log_dir $output_path \
+    --model mae_vit_base_patch16 \
+    --warmup_epochs 40 \
+    --epochs 400 \
+    --log_freq 40 \
+    --vae \
+    --kld_beta 25 \
+    --invisible_lr_scale 0.01 \
+    --mask_ratio 0.75 \
+    --partial_vae \
+    --dropout_ratio 0 \
+    --eps 1e-8 \
+    --weight_decay 0.05 \
+    --mixed_precision \
+    --wandb_project imagenet_scaledUp \
+    --wandb_name initialMultiNodeTry \
+    --disable_zero_conv \
+    --object_mask \
+    --add_default_mask \
+    --var 1 \
+    --exclude 'm[012]' \
+    --nodelist 'm[004,006]'
