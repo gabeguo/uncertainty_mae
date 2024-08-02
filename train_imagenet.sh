6c1a9d6e368be5dfa90421f1ce668c620678ac61
Gabe Guo <gzg2104@columbia.edu>
2024-08-02 09:43:19 -0400

    Revert to original settings (beta 30, lr scale 0.01, eps 1e-8); BUT have only 1 accum iteration; 4 GPU

--- train_imagenet.sh
+++ train_imagenet.sh
@@ -1,10 +1,10 @@
-output_path=/local/zemel/gzg2104/_imagenet_models/08_01_24/beta20_eps1e-6
+output_path=/local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch
 python main_pretrain.py \
     --dataset_name imagenet \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --batch_size 256 \
     --blr 1.5e-4 \
-    --accum_iter 4 \
+    --accum_iter 1 \
     --output_dir $output_path \
     --log_dir $output_path \
     --model mae_vit_base_patch16 \
@@ -13,12 +13,12 @@ python main_pretrain.py \
     --log_freq 20 \
     --num_workers 16 \
     --vae \
-    --kld_beta 20 \
-    --invisible_lr_scale 1e-2 \
+    --kld_beta 30 \
+    --invisible_lr_scale 0.01 \
     --mask_ratio 0.75 \
     --partial_vae \
     --dropout_ratio 0 \
-    --eps 1e-6 \
+    --eps 1e-8 \
     --weight_decay 0.05 \
     --mixed_precision \
     --wandb_project imagenet_hippo \
