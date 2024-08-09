0fc7d00aa737939cef4a9817fbbdc6280ec38ff5
Gabe Guo <gzg2104@columbia.edu>
2024-08-09 12:04:57 -0400

    end-to-end finetune

--- linprobe_uncertainty.sh
+++ linprobe_uncertainty.sh
@@ -4,8 +4,8 @@ python main_linprobe.py \
     --dataset_name cifar \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --nb_classes 100 \
-    --output_dir /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/linprobe/600 \
-    --batch_size 1024 \
+    --output_dir /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/end_to_end_finetune/600 \
+    --batch_size 512 \
     --log_dir /local/zemel/gzg2104/logs \
     --wandb_project linprobe_cifar \
     --device cuda \
