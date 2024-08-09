372775a4fe3a0be0ac2735799a0f0b6d15814040
Gabe Guo <gzg2104@columbia.edu>
2024-08-09 17:41:01 -0400

    finetune baseline end-to-end on CIFAR

--- finetune_uncertainty.sh
+++ finetune_uncertainty.sh
@@ -1,14 +1,14 @@
 python main_finetune.py \
     --model vit_base_patch16 \
-    --finetune /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/checkpoint-600.pth \
+    --finetune /home/gzg2104/uncertainty_mae/mae_visualize_vit_base.pth \
     --dataset_name cifar \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --nb_classes 100 \
-    --output_dir /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/finetune_end_to_end/600 \
+    --output_dir /local/zemel/gzg2104/_baseline_finetune_end_to_end/cifar_08_09_24 \
     --batch_size 512 \
     --accum_iter 2 \
     --log_dir /local/zemel/gzg2104/logs \
     --wandb_project linprobe_cifar \
     --device cuda \
     --num_workers 8 \
-    --master_port 12358
+    --master_port 12357
