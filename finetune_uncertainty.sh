2e0b60c9a2c91ae01fed3a8d24ff00e65d26c34b
Gabe Guo <gzg2104@columbia.edu>
2024-08-09 17:17:53 -0400

    8 workers

--- finetune_uncertainty.sh
+++ finetune_uncertainty.sh
@@ -10,5 +10,5 @@ python main_finetune.py \
     --log_dir /local/zemel/gzg2104/logs \
     --wandb_project linprobe_cifar \
     --device cuda \
-    --num_workers 16 \
+    --num_workers 8 \
     --master_port 12358
