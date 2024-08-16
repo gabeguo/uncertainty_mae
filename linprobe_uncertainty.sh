f0ae2f6e850027093e0c20640c292ed4197b90cd
Gabe Guo <gzg2104@columbia.edu>
2024-08-16 16:23:44 -0400

    linprobing the invisible head

--- linprobe_uncertainty.sh
+++ linprobe_uncertainty.sh
@@ -1,12 +1,13 @@
 python main_linprobe.py \
     --model vit_base_patch16 \
-    --finetune /local/zemel/gzg2104/_imagenet_models/08_10_24/batch1024_beta35/checkpoint-100.pth \
+    --finetune /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/checkpoint-600.pth \
     --dataset_name cifar \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --nb_classes 100 \
-    --output_dir /local/zemel/gzg2104/_imagenet_models/08_10_24/batch1024_beta35/linprobe/100 \
+    --output_dir /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/linprobe_INVISIBLE/600 \
     --batch_size 1024 \
     --log_dir /local/zemel/gzg2104/logs \
     --wandb_project linprobe_cifar \
     --device cuda \
-    --master_port 12358
+    --invisible_mae \
+    --master_port 12357
