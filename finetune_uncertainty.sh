c1fe4abceeb568c0926db977c378ee31b8c3a81c
Gabe Guo <gzg2104@columbia.edu>
2024-08-20 14:14:48 -0400

    finetune imagenet model on imagenet

--- finetune_uncertainty.sh
+++ finetune_uncertainty.sh
@@ -1,15 +1,14 @@
 python main_finetune.py \
     --model vit_base_patch16 \
     --finetune /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/checkpoint-600.pth \
-    --dataset_name cifar \
+    --dataset_name imagenet \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
-    --nb_classes 100 \
-    --output_dir /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/finetune_end_to_end_INVISIBLE/600 \
-    --batch_size 512 \
-    --accum_iter 2 \
+    --nb_classes 1000 \
+    --output_dir /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/imagenet_finetune/600 \
+    --batch_size 256 \
+    --accum_iter 1 \
     --log_dir /local/zemel/gzg2104/logs \
-    --wandb_project linprobe_cifar \
+    --wandb_project linprobe_imagenet \
     --device cuda \
     --num_workers 8 \
-    --master_port 12354 \
-    --invisible_mae
+    --master_port 12354
