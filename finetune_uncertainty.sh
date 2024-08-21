1b734b4dd60c834b2486fb3953640c1cd19d26e0
Gabe Guo <gzg2104@columbia.edu>
2024-08-21 15:13:45 -0400

    Resume finetuning full image evaluation, but scaled down to 2 GPUs

--- finetune_uncertainty.sh
+++ finetune_uncertainty.sh
@@ -1,15 +1,16 @@
 python main_finetune.py \
     --model vit_base_patch16 \
-    --finetune /home/gzg2104/uncertainty_mae/pretrained_models/mae_pretrain_vit_base.pth \
+    --finetune /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/checkpoint-600.pth \
     --dataset_name imagenet \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --nb_classes 1000 \
-    --output_dir /local/zemel/gzg2104/_baseline_finetune_end_to_end/imageNet_partialImage \
-    --batch_size 512 \
-    --accum_iter 1 \
+    --output_dir /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/imagenet_finetune/600 \
+    --batch_size 256 \
+    --accum_iter 2 \
     --log_dir /local/zemel/gzg2104/logs \
     --wandb_project linprobe_imagenet \
     --device cuda \
     --num_workers 8 \
-    --master_port 12357 \
-    --keep_ratio 0.25
+    --master_port 12354 \
+    --resume /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/imagenet_finetune/600/checkpoint-60.pth \
+    --log_freq 5
