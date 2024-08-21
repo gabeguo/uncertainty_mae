b6f449729a8823a75dd7bc77ea2a1979a0f0c19f
Gabe Guo <gzg2104@columbia.edu>
2024-08-21 12:42:10 -0400

    have inpainting and object detection scripts

--- generate_inpaintings.sh
+++ generate_inpaintings.sh
@@ -1,8 +1,8 @@
 python generate_inpaintings.py \
     --uncertainty_weights /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/checkpoint-600.pth \
     --baseline_weights /home/gzg2104/uncertainty_mae/mae_visualize_vit_base.pth \
-    --num_iterations 20 \
-    --num_samples 3 \
-    --save_dir /local/zemel/gzg2104/outputs/08_20_24_dummy3 \
+    --num_iterations 5000 \
+    --num_samples 2 \
+    --save_dir /local/zemel/gzg2104/outputs/08_21_24/inpaintings \
     --max_mask_ratio 0.6 \
     --min_mask_ratio 0.2
--- object_detection.sh
+++ object_detection.sh
@@ -1,4 +1,4 @@
 python object_detection.py \
-    --input_dir /local/zemel/gzg2104/outputs/08_20_24_dummy4 \
-    --output_dir /local/zemel/gzg2104/outputs/08_20_24_dummy_detection \
+    --input_dir /local/zemel/gzg2104/outputs/08_21_24/inpaintings \
+    --output_dir /local/zemel/gzg2104/outputs/08_21_24/detection \
     --box_score_thresh 0.4
