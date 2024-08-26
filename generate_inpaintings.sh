22e982fdc3eef0a9384f36d8e3c9fd7bceb3d691
Gabe Guo <gzg2104@columbia.edu>
2024-08-26 14:15:14 -0400

    Generate inpaintings at four samples each

--- generate_inpaintings.sh
+++ generate_inpaintings.sh
@@ -1,9 +1,17 @@
+num_samples=4
+img_dir=/local/zemel/gzg2104/outputs/08_26_24/inpaintings
+
 python generate_inpaintings.py \
-    --uncertainty_weights /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/checkpoint-600.pth \
+    --uncertainty_weights /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/checkpoint-799.pth \
     --baseline_weights /home/gzg2104/uncertainty_mae/mae_visualize_vit_base.pth \
     --num_iterations 5000 \
-    --num_samples 2 \
-    --save_dir /local/zemel/gzg2104/outputs/08_21_24/inpaintings \
+    --num_samples $num_samples \
+    --save_dir $img_dir \
     --max_mask_ratio 0.6 \
-    --min_mask_ratio 0.2 \
-    --start_from 2593
+    --min_mask_ratio 0.2
+
+python object_detection.py \
+    --input_dir $img_dir \
+    --output_dir /local/zemel/gzg2104/outputs/08_26_24/detection_mAP \
+    --box_score_thresh 0.4 \
+    --num_trials_inpaint $num_samples
