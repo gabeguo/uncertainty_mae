fc1391bb33a664e88086f74156769f4edad435b9
Gabe Guo <gzg2104@columbia.edu>
2024-08-26 12:13:11 -0400

    Have a sampling-based mAP evaluation, where we take the best mAP

--- object_detection.py
+++ object_detection.py
@@ -13,6 +13,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm, Normalize
 
+import json
+
+from torchmetrics.detection import MeanAveragePrecision
+from pprint import pprint
+
 CATEGORIES = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT.meta["categories"]
 
 def process_image(args, img_path, model):
@@ -43,19 +48,55 @@ def process_image(args, img_path, model):
     # print(labels)
     # print(label_nums)
 
-    return [value.item() for value in label_nums]
+    return [value.item() for value in label_nums], {key:prediction[key].detach().cpu() for key in prediction}
 
-def calc_co_occurrence(args, dir, model):
+def get_img_num(img_name):
+    the_num = int(img_name.split('_')[0])
+    return the_num
+
+def calc_co_occurrence(args, dir, model, num_trials_per_image=1):
     co_occurrence = np.zeros((91, 91))
+    img_num_to_prediction = dict()
     for img_name in tqdm(os.listdir(dir)):
         img_path = os.path.join(dir, img_name)
-        label_nums = process_image(args, img_path=os.path.join(dir, img_name), model=model)
-        # print(label_nums)
-        for idx_i, label_i in enumerate(label_nums):
-            for idx_j, label_j in enumerate(label_nums):
-                if idx_i != idx_j: # don't want object to co-occur with itself
-                    co_occurrence[label_i, label_j] += 1
-    return co_occurrence
+        the_img_num = get_img_num(img_name)
+        for _ in range(num_trials_per_image): # sample baseline image multiple times, to be fair
+            label_nums, prediction = process_image(args, img_path=os.path.join(dir, img_name), model=model)
+            # keep track of the predictions
+            if the_img_num not in img_num_to_prediction:
+                img_num_to_prediction[the_img_num] = list()
+            img_num_to_prediction[the_img_num].append(prediction)
+            # keep track of co-occurrence
+            for idx_i, label_i in enumerate(label_nums):
+                for idx_j, label_j in enumerate(label_nums):
+                    if idx_i != idx_j: # don't want object to co-occur with itself
+                        co_occurrence[label_i, label_j] += 1
+    return co_occurrence, img_num_to_prediction
+
+def get_best_map(gt_detections, pred_detections):
+    best_map = 0
+    for i in range(len(pred_detections)):
+        metric = MeanAveragePrecision(iou_type="bbox") # reset each time
+        metric.update(gt_detections, pred_detections[i:i+1])
+        assert len(metric.detection_labels) == 1
+        best_map = max(best_map, metric.compute()['map'].item())
+    return best_map
+
+def get_map(gt, ours, baseline):
+    assert len(gt) == len(ours) == len(baseline)
+    maps_ours = list()
+    maps_baseline = list()
+    for img_num in tqdm(gt):
+        gt_detections = gt[img_num]
+        assert len(gt_detections) == 1
+        # multi-trial
+        assert len(ours[img_num]) == len(baseline[img_num]), \
+            f"{img_num}; ours: {ours[img_num]}; baseline: {baseline[img_num]}"
+        # ours
+        maps_ours.append(get_best_map(gt_detections, ours[img_num]))
+        maps_baseline.append(get_best_map(gt_detections, baseline[img_num]))
+
+    return (np.mean(maps_ours), np.std(maps_ours)), (np.mean(maps_baseline), np.std(maps_baseline))
 
 def save_co_occurrence(args, co_occurrence, name):
     # save np
@@ -78,11 +119,23 @@ def save_co_occurrence(args, co_occurrence, name):
 
     return
 
+def save_stats(args, map_ours, map_baseline):
+    with open(os.path.join(args.output_dir, 'params.json'), 'w') as fout:
+        json.dump(vars(args), fout, indent=4)
+    with open(os.path.join(args.output_dir, 'maps.json'), 'w') as fout:
+        json.dump(
+            {'map_ours': map_ours, 'map_baseline': map_baseline},
+            fout, indent=4
+        )
+
+    return
+
 def create_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_dir', type=str)
     parser.add_argument('--output_dir', type=str)
     parser.add_argument('--box_score_thresh', type=float, default=0.7)
+    parser.add_argument('--num_trials_inpaint', type=int, default=2)
 
     args = parser.parse_args()
 
@@ -101,15 +154,23 @@ def main(args):
     model.eval()
     model = model.cuda()
 
-    co_occurrence_gt = calc_co_occurrence(args, dir=gt_dir, model=model)
-    co_occurrence_ours = calc_co_occurrence(args, dir=inpaint_ours_dir, model=model)
-    co_occurrence_baseline = calc_co_occurrence(args, dir=inpaint_baseline_dir, model=model)
+    co_occurrence_gt, gt_img_to_pred = calc_co_occurrence(args, dir=gt_dir, 
+        model=model, num_trials_per_image=1)
+    co_occurrence_ours, ours_img_to_pred = calc_co_occurrence(args, dir=inpaint_ours_dir, 
+        model=model, num_trials_per_image=1)
+    co_occurrence_baseline, baseline_img_to_pred = calc_co_occurrence(args, dir=inpaint_baseline_dir, 
+        model=model, num_trials_per_image=args.num_trials_inpaint)
 
+    map_ours, map_baseline = get_map(gt=gt_img_to_pred, 
+        ours=ours_img_to_pred, baseline=baseline_img_to_pred)
+
+    print(f'Our mAP: {map_ours[0]:.3f} +/- {map_ours[1]:.3f}')
+    print(f'Baseline mAP: {map_baseline[0]:.3f} +/- {map_baseline[1]:.3f}')
+    
     save_co_occurrence(args, co_occurrence_gt, 'co_occurrence_gt')
     save_co_occurrence(args, co_occurrence_ours, 'co_occurrence_ours')
     save_co_occurrence(args, co_occurrence_baseline, 'co_occurrence_baseline')
-    
-    # TODO: compare
+    save_stats(args, map_ours=map_ours, map_baseline=map_baseline)
 
     return
 
