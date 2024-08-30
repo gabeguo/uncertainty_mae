ad9edd01a5c6657e92d4c0edfcd11b06e9c05c30
Gabe Guo <gzg2104@columbia.edu>
2024-08-29 20:58:05 -0400

    Train with 4 GPUs, batch size 256 on each of them (no gradient accumulation); from epoch 180, kill on 251; added kill switch

--- train_imagenet.sh
+++ train_imagenet.sh
@@ -4,7 +4,7 @@ python main_pretrain.py \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --batch_size 256 \
     --blr 1.5e-4 \
-    --accum_iter 2 \
+    --accum_iter 1 \
     --output_dir $output_path \
     --log_dir $output_path \
     --model mae_vit_base_patch16 \
@@ -28,4 +28,5 @@ python main_pretrain.py \
     --add_default_mask \
     --var 1 \
     --norm_pix_loss \
-    --resume /local/zemel/gzg2104/_imagenet_models/08_26_24/normPixLoss/checkpoint-170.pth
+    --resume /local/zemel/gzg2104/_imagenet_models/08_26_24/normPixLoss/checkpoint-180.pth \
+    --kill_epoch 251
