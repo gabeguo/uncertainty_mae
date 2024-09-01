3b58eb4246733e6fd90bf0e1762719875aba7327
Gabe Guo <gzg2104@columbia.edu>
2024-09-01 17:49:20 -0400

    Finetune full model (visualization-compatible) from checkpoint 15

--- finetune_uncertainty.sh
+++ finetune_uncertainty.sh
@@ -1,10 +1,10 @@
 python main_finetune.py \
     --model vit_base_patch16 \
-    --finetune /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/checkpoint-420.pth \
+    --finetune /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/checkpoint-799.pth \
     --dataset_name imagenet \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --nb_classes 1000 \
-    --output_dir /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/finetune_compareToNorm/420 \
+    --output_dir /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/imagenet_finetune_diffParams/799 \
     --batch_size 256 \
     --accum_iter 2 \
     --blr 5e-4 \
@@ -15,4 +15,5 @@ python main_finetune.py \
     --device cuda \
     --num_workers 8 \
     --master_port 12358 \
-    --log_freq 5
+    --log_freq 5 \
+    --resume /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/imagenet_finetune_diffParams/799/checkpoint-15.pth
