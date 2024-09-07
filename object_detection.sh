f4e561c22dd27d98a8d3ed71453e0788ed92f6d8
Gabe Guo <gzg2104@columbia.edu>
2024-09-07 16:48:48 -0400

    proper output dir

--- object_detection.sh
+++ object_detection.sh
@@ -1,5 +1,5 @@
 python object_detection.py \
     --input_dir /local/zemel/gzg2104/outputs/09_06_24/inpaintings \
-    --output_dir /local/zemel/gzg2104/outputs/09_07_24/dummyObjectDetection \
+    --output_dir /local/zemel/gzg2104/outputs/09_07_24/objectDetectionCroppedPics \
     --box_score_thresh 0.6 \
     --occurrence_prob_threshold 0.1
