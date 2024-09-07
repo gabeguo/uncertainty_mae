fd70b5f46d079499f5964af99455b514b5b5f18e
Gabe Guo <gzg2104@columbia.edu>
2024-09-07 16:47:30 -0400

    tighten up threshold

--- object_detection.sh
+++ object_detection.sh
@@ -1,5 +1,5 @@
 python object_detection.py \
     --input_dir /local/zemel/gzg2104/outputs/09_06_24/inpaintings \
     --output_dir /local/zemel/gzg2104/outputs/09_07_24/dummyObjectDetection \
-    --box_score_thresh 0.5 \
+    --box_score_thresh 0.6 \
     --occurrence_prob_threshold 0.1
