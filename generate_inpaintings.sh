fbedb7df0a5eda60b09aa683fdb6f7045c7ffdc3
Gabe Guo <gzg2104@columbia.edu>
2024-09-22 13:46:44 -0400

    bump up threshold to 0.8 (so we don't get nonsensical paintings); also have option to do single-sample eval

--- generate_inpaintings.sh
+++ generate_inpaintings.sh
@@ -1,4 +1,4 @@
-img_dir=/local/zemel/gzg2104/outputs/09_20_24/eight_samples_save_mask
+img_dir=/local/zemel/gzg2104/outputs/09_19_24/four_samples_save_mask
 
 # python generate_inpaintings.py \
 #     --uncertainty_weights /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/checkpoint-799.pth \
@@ -11,6 +11,13 @@ img_dir=/local/zemel/gzg2104/outputs/09_20_24/eight_samples_save_mask
 
 python object_detection.py \
     --input_dir $img_dir \
-    --output_dir /local/zemel/gzg2104/outputs/09_20_24/eight_samples_save_mask_objectDetection_maskedBEiT \
-    --box_score_thresh 0.6 \
+    --output_dir /local/zemel/gzg2104/outputs/09_22_24/objectDetectionMaskedBEiT/four/thresh0_8/multiSampleBEiT \
+    --box_score_thresh 0.8 \
     --occurrence_prob_threshold 0.1
+
+python object_detection.py \
+    --input_dir $img_dir \
+    --output_dir /local/zemel/gzg2104/outputs/09_22_24/objectDetectionMaskedBEiT/four/thresh0_8/singleSampleBEiT \
+    --box_score_thresh 0.8 \
+    --occurrence_prob_threshold 0.1 \
+    --single_sample_beit
