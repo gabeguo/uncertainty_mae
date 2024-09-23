920e47270825715b5a910731ab4f95c63bd2161d
Gabe Guo <gzg2104@columbia.edu>
2024-09-22 20:22:20 -0400

    single-sample BEiT

--- generate_inpaintings.sh
+++ generate_inpaintings.sh
@@ -9,15 +9,22 @@ img_dir=/local/zemel/gzg2104/outputs/09_19_24/four_samples_save_mask
 #     --max_mask_ratio 1 \
 #     --min_mask_ratio 0
 
-python object_detection.py \
-    --input_dir $img_dir \
-    --output_dir /local/zemel/gzg2104/outputs/09_22_24/objectDetectionMaskedBEiT/four/thresh0_8/multiSampleBEiT \
-    --box_score_thresh 0.8 \
-    --occurrence_prob_threshold 0.1
+# python object_detection.py \
+#     --input_dir $img_dir \
+#     --output_dir /local/zemel/gzg2104/outputs/09_22_24/objectDetectionMaskedBEiT/four/thresh0_8/multiSampleBEiT \
+#     --box_score_thresh 0.8 \
+#     --occurrence_prob_threshold 0.1
 
 python object_detection.py \
     --input_dir $img_dir \
-    --output_dir /local/zemel/gzg2104/outputs/09_22_24/objectDetectionMaskedBEiT/four/thresh0_8/singleSampleBEiT \
-    --box_score_thresh 0.8 \
+    --output_dir /local/zemel/gzg2104/outputs/09_22_24/objectDetectionMaskedBEiT/four/thresh0_6/singleSampleBEiTSoftmax \
+    --box_score_thresh 0.6 \
     --occurrence_prob_threshold 0.1 \
-    --single_sample_beit
+    --single_sample_beit --skip_mae
+
+python object_detection.py \
+    --input_dir $img_dir \
+    --output_dir /local/zemel/gzg2104/outputs/09_22_24/objectDetectionMaskedBEiT/four/thresh0_6/singleSampleBEiTOneHot \
+    --box_score_thresh 0.6 \
+    --occurrence_prob_threshold 0.1 \
+    --single_sample_beit --skip_mae --beit_one_hot
