b7ca2244a1b807fc9ae2be3e1623840d48c9cc13
Gabe Guo <gzg2104@columbia.edu>
2024-11-07 13:00:28 -0500

    (3) Lower lr 10-fold; (4) Do not do gradient accumulation, since htat may affect the logic; (5) Have 3 warmup epochs out of 15; (6) decrease the GAN lambda to 0.05

--- train_gan.sh
+++ train_gan.sh
@@ -1,4 +1,4 @@
-output_path=/local/zemel/gzg2104/_imagenet_models/11_06_24/gan_lambda_1
+output_path=/local/zemel/gzg2104/_imagenet_models/11_07_24/occasional_gan
 
 echo "train gan!"
 
@@ -6,13 +6,13 @@ CUDA_VISIBLE_DEVICES=7 python main_pretrain.py \
     --dataset_name imagenet \
     --data_path /local/zemel/gzg2104/datasets/imagenet \
     --batch_size 256 \
-    --blr 1.5e-4 \
-    --accum_iter 2 \
+    --blr 1.5e-5 \
+    --accum_iter 1 \
     --output_dir $output_path \
     --log_dir $output_path \
     --model mae_vit_base_patch16 \
-    --warmup_epochs 1 \
-    --epochs 10 \
+    --warmup_epochs 3 \
+    --epochs 15 \
     --log_freq 1 \
     --num_workers 8 \
     --vae \
@@ -20,15 +20,15 @@ CUDA_VISIBLE_DEVICES=7 python main_pretrain.py \
     --invisible_lr_scale 0.01 \
     --mask_ratio 0.75 \
     --partial_vae \
-    --dropout_ratio 1 \
+    --dropout_ratio 0.5 \
     --eps 1e-8 \
     --weight_decay 0.05 \
     --mixed_precision \
     --wandb_project imagenet_hippo \
     --disable_zero_conv \
-    --master_port 12356 \
+    --master_port 12357 \
     --object_mask \
     --add_default_mask \
     --var 1 \
     --resume /local/zemel/gzg2104/_imagenet_models/08_02_24/revertSmallBatch/checkpoint-799.pth \
-    --gan --gan_lambda 1
+    --gan --gan_lambda 0.05
